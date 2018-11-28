64c601f7daf2b9c2f5bb995e5d2c4e4d11cc33bb
Garrett Straight <41503255+GarrettStraight@users.noreply.github.com>
2018-11-27 23:40:49 -0800

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -85,7 +85,7 @@ database.ref().on("child_added", function(snapshot) {
       $("#trainFrequency").append(newFreq);
       $("#trainMinutesAway").append(newMinutes);
 
-      var next = currentTime + parseInt(theFutureTime);
+      var next = currentTime + parseint(theFutureTime);
 
       console.log(next);
       console.log("The Current Time is: " + currentTime);
